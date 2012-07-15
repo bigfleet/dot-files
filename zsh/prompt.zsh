0942fe6913e425e4e28579561aa80ebc14e282f2
bigfleet <jim@jimvanfleet.com>
2012-07-14 20:21:18 -0400

    Removing the unused (for me) pair element of prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -5,12 +5,6 @@ function parse_git_branch() {
 function git_prompt_info() {
   local ref=$(git symbolic-ref HEAD 2> /dev/null)
   local gitst="$(git status 2> /dev/null)"
-  local pairname=$(git config --get user.initials)
-  if [[ ${pairname} == '' ]]; then
-    pairname='(solo)'
-  else
-    pairname="($pairname)"
-  fi
 
   if [[ -f .git/MERGE_HEAD ]]; then
     if [[ ${gitst} =~ "unmerged" ]]; then
@@ -28,7 +22,7 @@ function git_prompt_info() {
     gitstatus=' '
   fi
   if [[ -n $ref ]]; then
-    echo "%{$fg_bold[green]%}/${ref#refs/heads/}%{$reset_color%}$gitstatus $pairname"
+    echo "%{$fg_bold[green]%}/${ref#refs/heads/}%{$reset_color%}$gitstatus"
   fi
 }
 
